a1a1bd42a7cd7c270271d553af18ee4cebccd6ed
VladyslavBondyk <vladyslav.bondyk@gmail.com>
2023-07-18 00:39:34 +0200

    added secure endpoints

--- src/main/java/com/ecommercebackend/api/security/WebSecurityConfig.java
+++ src/main/java/com/ecommercebackend/api/security/WebSecurityConfig.java
@@ -6,16 +6,32 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 
-@EnableWebSecurity
 @Configuration
 public class WebSecurityConfig {
 
+    private JWTRequestFilter jwtRequestFilter;
+
+    public WebSecurityConfig(JWTRequestFilter jwtRequestFilter) {
+        this.jwtRequestFilter = jwtRequestFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()).cors(cors -> cors.disable());
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.csrf().disable().cors().disable();
+        // We need to make sure our authentication filter is run before the http request filter is run.
+        http.addFilterBefore(jwtRequestFilter, AuthorizationFilter.class);
+        AuthorizeHttpRequestsConfigurer<HttpSecurity>
+                .AuthorizationManagerRequestMatcherRegistry authenticated
+                = http.authorizeHttpRequests()
+                // Specific exclusions or rules.
+                .requestMatchers("/product", "/auth/signup", "/auth/login").permitAll()
+                // Everything else should be authenticated.
+                .anyRequest().authenticated();
         return http.build();
     }
 
