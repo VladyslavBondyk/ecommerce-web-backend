04e5b5d8ca248bb103981b3402562b2f69fe341c
VladyslavBondyk <vladyslav.bondyk@gmail.com>
2023-07-31 23:16:40 +0200

    updated structure

--- src/test/java/com/ecommercebackend/service/UserServiceTest.java
+++ src/test/java/com/ecommercebackend/service/UserServiceTest.java
@@ -1,93 +1,93 @@
-package com.ecommercebackend.service;
-
-import com.ecommercebackend.api.model.LoginBody;
-import com.ecommercebackend.api.model.RegistrationBody;
-import com.ecommercebackend.exception.EmailFailureException;
-import com.ecommercebackend.exception.UserAlreadyExistsException;
-import com.ecommercebackend.exception.UserNotVerifiedException;
-import com.icegreen.greenmail.configuration.GreenMailConfiguration;
-import com.icegreen.greenmail.junit5.GreenMailExtension;
-import com.icegreen.greenmail.util.ServerSetup;
-import com.icegreen.greenmail.util.ServerSetupTest;
-import jakarta.mail.Message;
-import jakarta.mail.MessageAware;
-import jakarta.mail.MessagingException;
-import jakarta.transaction.Transactional;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.extension.RegisterExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class UserServiceTest {
-
-    /** Extension for mocking email sending. */
-    @RegisterExtension
-    private static GreenMailExtension greenMailExtension = new GreenMailExtension(ServerSetupTest.SMTP)
-            .withConfiguration(GreenMailConfiguration.aConfig().withUser("springboot", "secret"))
-            .withPerMethodLifecycle(true);
-    /** The UserService to test. */
-    @Autowired
-    UserService userService;
-
-    /**
-     * Tests the registration process of the user.
-     * @throws MessagingException Thrown if the mocked email service fails somehow.
-     */
-    @Test
-    @Transactional
-    public void testRegisterUser() throws MessagingException {
-        RegistrationBody body = new RegistrationBody();
-        body.setEmail("UserServiceTest$testRegisterUser@junit.com");
-        body.setFirstName("FirstName");
-        body.setLastName("LastName");
-        body.setPassword("MySecretPassword123");
-        Assertions.assertThrows(UserAlreadyExistsException.class,
-                () -> userService.registerUser(body), "Email should already be in use.");
-        body.setEmail("UserA@junit.com");
-        Assertions.assertThrows(UserAlreadyExistsException.class,
-                () -> userService.registerUser(body), "Email should already be in use.");
-        body.setEmail("UserServiceTest$testRegisterUser@junit.com");
-        Assertions.assertDoesNotThrow(() -> userService.registerUser(body),
-                "User should register successfully.");
-        Assertions.assertEquals(body.getEmail(), greenMailExtension.getReceivedMessages()[0]
-                .getRecipients(Message.RecipientType.TO)[0].toString());
-    }
-
-    /**
-     * Tests the loginUser method.
-     * @throws UserNotVerifiedException
-     * @throws EmailFailureException
-     */
-    @Test
-    @Transactional
-    public void testLoginUser() throws UserNotVerifiedException, EmailFailureException {
-        LoginBody body = new LoginBody();
-        body.setEmail("UserA-NotExists@junit.com");
-        body.setPassword("PasswordA123-BadPassword");
-        Assertions.assertNull(userService.loginUser(body), "The user should not exist.");
-        body.setEmail("UserA@junit.com");
-        Assertions.assertNull(userService.loginUser(body), "The password should be incorrect.");
-        body.setPassword("PasswordA123");
-        Assertions.assertNotNull(userService.loginUser(body), "The user should login successfully.");
-        body.setEmail("UserB@junit.com");
-        body.setPassword("PasswordB123");
-        try {
-            userService.loginUser(body);
-            Assertions.assertTrue(false, "User should not have email verified.");
-        } catch (UserNotVerifiedException ex) {
-            Assertions.assertTrue(ex.isNewEmailSent(), "Email verification should be sent.");
-            Assertions.assertEquals(1, greenMailExtension.getReceivedMessages().length);
-        }
-        try {
-            userService.loginUser(body);
-            Assertions.assertTrue(false, "User should not have email verified.");
-        } catch (UserNotVerifiedException ex) {
-            Assertions.assertFalse(ex.isNewEmailSent(), "Email verification should not be resent.");
-            Assertions.assertEquals(1, greenMailExtension.getReceivedMessages().length);
-        }
-    }
-
-}
-
+//package com.ecommercebackend.service;
+//
+//import com.ecommercebackend.api.model.LoginBody;
+//import com.ecommercebackend.api.model.RegistrationBody;
+//import com.ecommercebackend.exception.EmailFailureException;
+//import com.ecommercebackend.exception.UserAlreadyExistsException;
+//import com.ecommercebackend.exception.UserNotVerifiedException;
+//import com.icegreen.greenmail.configuration.GreenMailConfiguration;
+//import com.icegreen.greenmail.junit5.GreenMailExtension;
+//import com.icegreen.greenmail.util.ServerSetup;
+//import com.icegreen.greenmail.util.ServerSetupTest;
+//import jakarta.mail.Message;
+//import jakarta.mail.MessageAware;
+//import jakarta.mail.MessagingException;
+//import jakarta.transaction.Transactional;
+//import org.junit.Test;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.extension.RegisterExtension;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class UserServiceTest {
+//
+//    /** Extension for mocking email sending. */
+//    @RegisterExtension
+//    private static GreenMailExtension greenMailExtension = new GreenMailExtension(ServerSetupTest.SMTP)
+//            .withConfiguration(GreenMailConfiguration.aConfig().withUser("springboot", "secret"))
+//            .withPerMethodLifecycle(true);
+//    /** The UserService to test. */
+//    @Autowired
+//    UserService userService;
+//
+//    /**
+//     * Tests the registration process of the user.
+//     * @throws MessagingException Thrown if the mocked email service fails somehow.
+//     */
+//    @Test
+//    @Transactional
+//    public void testRegisterUser() throws MessagingException {
+//        RegistrationBody body = new RegistrationBody();
+//        body.setEmail("UserServiceTest$testRegisterUser@junit.com");
+//        body.setFirstName("FirstName");
+//        body.setLastName("LastName");
+//        body.setPassword("MySecretPassword123");
+//        Assertions.assertThrows(UserAlreadyExistsException.class,
+//                () -> userService.registerUser(body), "Email should already be in use.");
+//        body.setEmail("UserA@junit.com");
+//        Assertions.assertThrows(UserAlreadyExistsException.class,
+//                () -> userService.registerUser(body), "Email should already be in use.");
+//        body.setEmail("UserServiceTest$testRegisterUser@junit.com");
+//        Assertions.assertDoesNotThrow(() -> userService.registerUser(body),
+//                "User should register successfully.");
+//        Assertions.assertEquals(body.getEmail(), greenMailExtension.getReceivedMessages()[0]
+//                .getRecipients(Message.RecipientType.TO)[0].toString());
+//    }
+//
+//    /**
+//     * Tests the loginUser method.
+//     * @throws UserNotVerifiedException
+//     * @throws EmailFailureException
+//     */
+//    @Test
+//    @Transactional
+//    public void testLoginUser() throws UserNotVerifiedException, EmailFailureException {
+//        LoginBody body = new LoginBody();
+//        body.setEmail("UserA-NotExists@junit.com");
+//        body.setPassword("PasswordA123-BadPassword");
+//        Assertions.assertNull(userService.loginUser(body), "The user should not exist.");
+//        body.setEmail("UserA@junit.com");
+//        Assertions.assertNull(userService.loginUser(body), "The password should be incorrect.");
+//        body.setPassword("PasswordA123");
+//        Assertions.assertNotNull(userService.loginUser(body), "The user should login successfully.");
+//        body.setEmail("UserB@junit.com");
+//        body.setPassword("PasswordB123");
+//        try {
+//            userService.loginUser(body);
+//            Assertions.assertTrue(false, "User should not have email verified.");
+//        } catch (UserNotVerifiedException ex) {
+//            Assertions.assertTrue(ex.isNewEmailSent(), "Email verification should be sent.");
+//            Assertions.assertEquals(1, greenMailExtension.getReceivedMessages().length);
+//        }
+//        try {
+//            userService.loginUser(body);
+//            Assertions.assertTrue(false, "User should not have email verified.");
+//        } catch (UserNotVerifiedException ex) {
+//            Assertions.assertFalse(ex.isNewEmailSent(), "Email verification should not be resent.");
+//            Assertions.assertEquals(1, greenMailExtension.getReceivedMessages().length);
+//        }
+//    }
+//
+//}
+//
